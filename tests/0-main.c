7563ae92ab6b2f5b51d7a8cfb0b957814defbdfc
Maryam <morrryyyy21@gmail.com>
2024-01-30 14:12:06 +0100

    add test files

---
+++ tests/0-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	_printf("My name is Mariam");
+	_printf("%s", "Mariam");
+	_printf("%c", 'h');
+	_printf("%%");
+	return (0);
+}
---
+++ tests/main.h~
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int print_str(char *s);
+
+#endif
